1f0744dcc5456183375fe5da5dedcfc3c57ff2ca
MiwaUS7605 <93576098+MiwaUS7605@users.noreply.github.com>
2021-12-09 00:56:58 +0700

    Update CConsole.h
    More function for screen display and objects

--- Quang/CConsole.h
+++ Quang/CConsole.h
@@ -2,6 +2,9 @@
 #define C_CONSOLE_
 
 #include<Windows.h>
+#include<vector>
+#include<fstream>
+#include<string>
 #include<iostream>
 using namespace std;
 
@@ -11,8 +14,12 @@ void SetScreenBufferSize(SHORT width, SHORT height);
 void ShowScrollbar(BOOL Show);
 void FixConsoleWindow();
 void ScreenSetting(int width, int height);
-void ShowCur(bool CursorVisibility);
+void ShowConsoleCursor(bool CursorVisibility);
+
+void getObjectSize(int& height, int& width, const vector<string>& objSprite);
+
+void ReadObjectSprite(string filename, vector<string>& objectSprite);
+void DrawObjectSprite(int x, int y, const vector<string>& objectSprite);
+void DeleteObjectSprite(int x, int y, const vector<string>& objectSprite);
 
 #endif // !C_CONSOLE_
-
-
