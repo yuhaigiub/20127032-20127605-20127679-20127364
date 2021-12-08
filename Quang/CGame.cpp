29fe4ab8e52c7f13aeba50bd1e62aefb2566388c
MiwaUS7605 <93576098+MiwaUS7605@users.noreply.github.com>
2021-12-09 01:20:54 +0700

    Update CGame.cpp
    Update InitializePosition function, randomize base on levels and difficulty

--- Quang/CGame.cpp
+++ Quang/CGame.cpp
@@ -1,8 +1,6 @@
 #include "CGame.h"
 
-CGame::CGame(int objectsPerRow, int width, int height) {
-	this->objectsPerRow = objectsPerRow;
-	
+CGame::CGame(int width, int height) {
 	// set border
 	this->topleft.SetX(1); this->topleft.SetY(1);
 	this->botright.SetX(width); this->botright.SetY(height);
@@ -20,90 +18,120 @@ CGame::CGame(int objectsPerRow, int width, int height) {
 	ReadObjectSprite(this->carSpritefile, this->carSprite);
 	ReadObjectSprite(this->tankSpritefile, this->tankSprite);
 	ReadObjectSprite(this->peopleSpritefile, this->peopleSprite);
-
-	//set objects initial position
-	InitializePosition();
 }
 
-void CGame::InitializePosition() {
-	int DEFAULT_BIRD_Y = 7;
-	int DEFAULT_CAR_Y = 11;
-	int DEFAULT_DINO_Y = 14;
-	int DEFAULT_TANK_Y = 19;
-	int DEFAULT_PEOPLE_Y = 25;
-	//bird
-	int w = 1, multiplier, x;
-	for (int i = 0; i < this->birdSprite.size(); i++)
-		if (w < this->birdSprite[i].size())
-			w = this->birdSprite[i].size();
-	x = this->botright.GetX() - w;
-	multiplier = this->botright.GetX() / w;
-
-	this->birds.clear();
+void CGame::InitializePosition(int &level) {
 	
-	for (int i = 0; i < this->objectsPerRow; i++) {
+	int w, h, multiplier, x;		// WRITE SEPARATE FUNCTIONS TO CALCULATE THESE
+
+	//random variable
+	int _rand;
+	int _diff_val = 0;
+	//initialize num and speed
+	// NUMBER OF ENEMY PER ROW FROM 1->7(MAX_ENEMY) (easy mode)
+	// NUMBER OF ENEMY PER ROW FROM 4->7(MAX_ENEMY) (hard mode)
+	if (this->difficult_state == true) {
+		_diff_val = rand() % (MAX_ENEMY - 4) + 3; //3->6
+	}
+	_rand = rand() % level + 1;
+	this->birdNum = _rand % (MAX_ENEMY - _diff_val) + 1 + _diff_val;
+	_rand = rand() % level;
+	this->dinoNum = _rand % (MAX_ENEMY - _diff_val) + 1 + _diff_val;
+	_rand = rand() % level;
+	this->carNum = _rand % (MAX_ENEMY - _diff_val) + 1 + _diff_val;
+	_rand = rand() % level + 1;
+	this->tankNum = _rand % (MAX_ENEMY - _diff_val) + 1 + _diff_val;
+
+	//SPEED RANDOM FROM 1->N-1 WHEN LESS THAN LEVEL X OF THE GAME
+	//AFTER LEVEL X MAINTAIN THE CONSTANT N SPEED
+	_rand = rand() % 3 + 1;
+	this->birdSpeed = level >= 10? 4: (level % _rand == 0 ? _rand + 1 : _rand);
+	_rand = rand() % 2 + 1;
+	this->dinoSpeed = level >= 15? 3 : (level % _rand == 0 ? _rand + 1 : _rand);
+	_rand = rand() % 2 + 1;
+	this->carSpeed = level >= 10? 4 : (level % _rand == 0 ? _rand + 1 : _rand);
+	_rand = rand() % 2 + 1;
+	this->tankSpeed = level >= 15? 3 : (level % _rand == 0 ? _rand + 1 : _rand);
+	this->people.SetSpeed(3, 4);	// DON'T RANDOM
+
+
+	//BIRD
+	//Get bird size (height and width from birdSprite, also hitbox)
+	getObjectSize(h, w, this->birdSprite);
+	//Starting point at the right/left (+/-1 because of the border)
+	//x = this->topleft.GetX() + 1 //for left side
+	x = this->botright.GetX() - w - 1; //right side
+	multiplier = (this->botright.GetX() - 2 - w) / w; //All possible positions
+
+	//Clear old data form the latest level
+	this->birds.clear();
+
+	//INITIAL POSITION OF ENEMY FOR NEW LEVEL
+	for (int i = 0; i < this->birdNum; i++) {
 		CBird* b = new CBird;
-		b->SetTopLeft(x - i * multiplier, DEFAULT_BIRD_Y); 
-		b->SetBotRight(b->GetTopLeft().GetX() + w, DEFAULT_BIRD_Y);
+		//Random the position
+		_rand = rand() % multiplier;
+		//Since start at right side so we let them minus
+		b->SetTopLeft(x - _rand*w, DEFAULT_BIRD_Y);
+		b->SetBotRight(b->GetTopLeft().GetX() + w, DEFAULT_BIRD_Y + h - 1);
 		this->birds.push_back(b);
 	}
 
-	//dino
-	w = 1;
-	for (int i = 0; i < this->dinosaurSprite.size(); i++)
-		if (w < this->dinosaurSprite[i].size())
-			w = this->dinosaurSprite[i].size();
-	x = this->botright.GetX() - w;
-	multiplier = this->botright.GetX() / w;
+	//Those 4 below also have the same explaination as the BIRD
+	
+	//DINO
+	getObjectSize(h, w, this->dinosaurSprite);
+	x = this->botright.GetX() - w - 1;
+	multiplier = (this->botright.GetX() - 2 - w) / w;
 
 	this->dinos.clear();
 
-	for (int i = 0; i < this->objectsPerRow; i++) {
+	for (int i = 0; i < this->dinoNum; i++) {
 		CDinosaur* d = new CDinosaur;
-		d->SetTopLeft(x - i * multiplier, DEFAULT_DINO_Y);
-		d->SetBotRight(d->GetTopLeft().GetX() + w, DEFAULT_DINO_Y);
+		_rand = rand() % multiplier;
+		d->SetTopLeft(x - _rand* w, DEFAULT_DINO_Y);
+		d->SetBotRight(d->GetTopLeft().GetX() + w, DEFAULT_DINO_Y + h - 1);
 		this->dinos.push_back(d);
 	}
 
-	//car
-	w = 1;
-	for (int i = 0; i < this->carSprite.size(); i++)
-		if (w < this->carSprite[i].size())
-			w = this->carSprite[i].size();
+	//CAR
+	getObjectSize(h, w, this->carSprite);
 	x = this->topleft.GetX() + 1;
-	multiplier = this->botright.GetX() / w;
+	multiplier = (this->botright.GetX() - 2 - w)/ w;
 
 	this->cars.clear();
 
-	for (int i = 0; i < this->objectsPerRow; i++) {
+	for (int i = 0; i < this->carNum; i++) {
 		CCar* c = new CCar;
-		c->SetTopLeft(x + i * multiplier, DEFAULT_CAR_Y);
-		c->SetBotRight(c->GetTopLeft().GetX() + w, DEFAULT_CAR_Y);
+		_rand = rand() % multiplier;
+		c->SetTopLeft(x + _rand * w, DEFAULT_CAR_Y);
+		c->SetBotRight(c->GetTopLeft().GetX() + w, DEFAULT_CAR_Y + h - 1);
 		this->cars.push_back(c);
 	}
 
-	//tank
-	w = 1;
-	for (int i = 0; i < this->tankSprite.size(); i++)
-		if (w < this->tankSprite[i].size())
-			w = this->tankSprite[i].size();
+	//TANK
+	getObjectSize(h, w, this->tankSprite);
 	x = this->topleft.GetX() + 1;
-	multiplier = this->botright.GetX() / w;
+	multiplier = (this->botright.GetX() - 2 - w) / w;
 
 	this->tanks.clear();
 
-	for (int i = 0; i < this->objectsPerRow; i++) {
+	for (int i = 0; i < this->tankNum; i++) {
 		CTank* t = new CTank;
-		t->SetTopLeft(x + i * multiplier, DEFAULT_TANK_Y);
-		t->SetBotRight(t->GetTopLeft().GetX() + w, DEFAULT_TANK_Y);
+		_rand = rand() % multiplier;
+		t->SetTopLeft(x + _rand * w, DEFAULT_TANK_Y);
+		t->SetBotRight(t->GetTopLeft().GetX() + w, DEFAULT_TANK_Y + h - 1);
 		this->tanks.push_back(t);
 	}
 
-	//People
-	this->people.SetTopLeft(2*this->botright.GetX() / 3,DEFAULT_PEOPLE_Y);
-	this->people.SetBotRight(this->people.GetTopLeft().GetX() + 3,DEFAULT_PEOPLE_Y + 3);
+	//PEOPLE
+	getObjectSize(h, w, this->peopleSprite);
+	x = this->topleft.GetX() + 1;
+	multiplier = (this->botright.GetX() - 2 - w) / w;
+	_rand = rand() % multiplier;
+	this->people.SetTopLeft(x + _rand * w, DEFAULT_PEOPLE_Y);
+	this->people.SetBotRight(this->people.GetTopLeft().GetX() + w, DEFAULT_PEOPLE_Y + h - 1);
 }
-
 CGame::~CGame() {
 	for (int i = 0; i < this->birds.size(); i++)
 		delete this->birds[i];
