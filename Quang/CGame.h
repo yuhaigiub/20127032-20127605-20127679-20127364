b1454f459abb59726efadefa1b06ddf9bf656790
MiwaUS7605 <93576098+MiwaUS7605@users.noreply.github.com>
2021-12-09 09:15:37 +0700

    Update CGame.h
    Adding more useful method prototypes

--- Quang/CGame.h
+++ Quang/CGame.h
@@ -31,8 +31,8 @@ private:
 	
 	//border
 	Point2D topleft;
-	Point2D botright; 
-
+	Point2D botright;
+	
 	//row state 
 	int birdNum, dinoNum, carNum, tankNum;
 	int birdSpeed, dinoSpeed, carSpeed, tankSpeed;
@@ -53,24 +53,28 @@ private:
 	string carSpritefile;
 	string tankSpritefile;
 	string peopleSpritefile;
-	
+	string RIPSpritefile;
+
+
 	vector<string> birdSprite;
 	vector<string> dinosaurSprite;
 	vector<string> carSprite;
 	vector<string> tankSprite;
 	vector<string> peopleSprite;
 	vector<string> RIPSprite;
+
 public:
-	
 	void SET_IS_RUNNING(bool state) { this->IS_RUNNING = state; }
 	bool GET_IS_RUNNING() { return this->IS_RUNNING; }
 	void SET_IS_PAUSING(bool state) { this->IS_PAUSING = state; }
 	bool GET_IS_PAUSING() { return this->IS_PAUSING; }
 
 	bool IS_FINISH();
-	
-	CGame(int width, int height); //initialize variables for the game
+
+
+	CGame(int width, int height, bool musicState); //initialize variables for the game
 	~CGame();
+
 	void InitializePosition(int&);
 	void UpdatePosTank();
 	void UpdatePosCar();
@@ -78,14 +82,31 @@ public:
 	void UpdatePosDino();
 	void UpdatePosPeople(char);
 
-	void ReadObjectSprite(string filename, vector<string>& objectSprite);
-	void DrawObjectSprite(int x, int y, const vector<string>& objectSprite);
-	void DeleteObjectSprite(int x, int y, const vector<string>& objectSprite);
 
+	//Draw
+	void drawTitle();
+	void RIP();
+	void LoadingScreen(string message = "");
 	void DrawBorder();
+
+	//Game Settings
+	void Setting();
+	void StartMenu();
+	void EndMenu();
+
+	void StartGame();
+
+	//
 	void Clean();
+	void GetInput();
 	void Update();
-	void Draw();
+	void Draw(); // main draw function
+
+
+	bool CheckCollision();
+	//
+	void save_game(string name);
+	bool load_game(string name);
 };
 
 #endif // !C_GAME_
