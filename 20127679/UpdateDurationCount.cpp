ee06ea29c474132e042931778bd3f9a2a4977e63
yuhaigiub <buigiahuynt@gmail.com>
2022-03-18 17:52:29 +0700

    Clean up repository, keep the FINAL product only

--- 20127679/UpdateDurationCount.cpp
+++
@@ -1,8 +0,0 @@
-//for every rotation with certain duration, update the status of car's traffic light and tank's traffic light (red/green)
-
-void CGame::UpdateDurationCount() {
-	//while (this->IS_RUNNING) {
-	carLight.UpdateDurationStatus();
-	tankLight.UpdateDurationStatus();
-	//}
-}
--- Quang/README.md
+++
@@ -1,3 +0,0 @@
-#### Important file/folder
-- Update_CGameCPP: a folder that update some functions in CGame.cpp
-- CGame.h: often updating
